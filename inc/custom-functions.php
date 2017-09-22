dc29d180f4574878753c894636fe2d47dbd560b5
kafleg <merohappylife@gmail.com>
2017-09-22 17:25:18 +0545

    First Commit

---
+++ inc/custom-functions.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Add ... for more view
+ *
+ * @since words 0.0.1
+ *
+ * @param null
+ * @return null
+ *
+ */
+
+if ( !function_exists('words_excerpt_more') ) :
+    function words_excerpt_more($more) { 
+
+    return '&hellip;';
+    }
+endif;
+add_filter('excerpt_more', 'words_excerpt_more');
+
+
+/**
+* Checkbox santitization
+*/
+if ( ! function_exists( 'words_sanitize_checkbox' ) ) :
+
+	function words_sanitize_checkbox( $checked ) {
+
+		return ( ( isset( $checked ) && true === $checked ) ? true : false );
+	}
+
+endif;
+
+
+/**
+ * Show/hide post meta 
+ */
+
+function words_show_post_meta(){
+	global $words_theme_options;
+    $hide_post_meta = absint($words_theme_options['words-blog-meta']);
+	if( $hide_post_meta == 0 ){
+		?>
+		<div class="entry-meta">
+			<?php 
+				words_posted_on();
+			?>
+		</div><!-- .entry-meta -->
+		<?php
+	}
+}
